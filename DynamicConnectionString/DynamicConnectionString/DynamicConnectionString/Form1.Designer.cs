20725c451b55dc1c8b6baf296b3275bb20ab21ce
MarkAguirre26 <markaguirre26@gmail.com>
2018-06-25 08:13:45 +0800

    Add files via upload

---
+++ DynamicConnectionString/DynamicConnectionString/DynamicConnectionString/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace DynamicConnectionString
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmdSet = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.cmdGet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(662, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Connection String";
+            // 
+            // cmdSet
+            // 
+            this.cmdSet.Location = new System.Drawing.Point(12, 61);
+            this.cmdSet.Name = "cmdSet";
+            this.cmdSet.Size = new System.Drawing.Size(75, 23);
+            this.cmdSet.TabIndex = 2;
+            this.cmdSet.Text = "Set";
+            this.cmdSet.UseVisualStyleBackColor = true;
+            this.cmdSet.Click += new System.EventHandler(this.cmdSet_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(662, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // cmdGet
+            // 
+            this.cmdGet.Location = new System.Drawing.Point(12, 117);
+            this.cmdGet.Name = "cmdGet";
+            this.cmdGet.Size = new System.Drawing.Size(75, 23);
+            this.cmdGet.TabIndex = 4;
+            this.cmdGet.Text = "Get";
+            this.cmdGet.UseVisualStyleBackColor = true;
+            this.cmdGet.Click += new System.EventHandler(this.cmdGet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 166);
+            this.Controls.Add(this.cmdGet);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.cmdSet);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cmdSet;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button cmdGet;
+
+    }
+}
+
---
+++ DynamicConnectionString/DynamicConnectionString/DynamicConnectionString/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace DynamicConnectionString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+   
+        void get()
+        {
+            textBox2.Text = Properties.Settings.Default.ConnectionString;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            get();
+        }
+
+        private void cmdSet_Click(object sender, EventArgs e)
+        {
+            /*
+           * Steps how to create "ConnectionString" in Properties
+           *1. Right click your project in solution explorer
+           *2. proceed to   Properties>Setting
+           *3. Under the Setting tab put ConnectionString or any name you want.
+           *4. Save
+           * * */
+            Properties.Settings.Default.ConnectionString = textBox1.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void cmdGet_Click(object sender, EventArgs e)
+        {
+            get();
+        }
+    }
+}
